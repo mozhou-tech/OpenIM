0c49a64c05fbdbd8c98cf986363af341e47b51f2
liuyuancheng <tenstone@foxmail.com>
2022-05-11 14:52:36 +0800

    init

--- chatboot-webgate/src/main/java/com/lesterlaucn/chatboot/WebGateSpringApplication.java
+++ chatboot-webgate/src/main/java/com/lesterlaucn/chatboot/WebGateSpringApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebGateSpringApplication
-{
+public class WebGateSpringApplication {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         SpringApplication.run(WebGateSpringApplication.class, args);
     }
 }
